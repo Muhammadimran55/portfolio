22777069bde9131c8d40479deabd26b8600ff62d
Muhammad Imran <imranmalik5322@gamil.com>
2026-01-31 17:36:25 +0500

    updated

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -6,8 +6,8 @@ export default function Contact(): JSX.Element {
       <h1>Contact Me</h1>
 
       <div className="card">
-        <p>Email: imran@email.com</p>
-        <p>Phone: +92 300 0000000</p>
+        <p>Email: imranmalik5322@gmail.com</p>
+        <p>Phone: +92 3326986806</p>
         <p>Location: Pakistan</p>
       </div>
     </>
